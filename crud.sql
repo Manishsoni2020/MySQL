21a8d4b1ec78a56fe78bbad8156f0358357b7862
Manishsoni2020 <sonimanish24163@gmail.com>
2024-09-12 16:40:20 +0530

    CREATE, DELETE, UPDATE, DROP

---
+++ crud.sql
@@ -0,0 +1,28 @@
+-- use flipcart
+-- show tables
+-- create table orders(order_id int,first_name varchar(20),last_name varchar(50))
+-- insert into orders values
+-- (1,"aarush","verma"),
+-- (2,"akshay","choudhary"),
+-- (3,"rakesh","mahla"),
+-- (4,"kunal","sharma")
+-- select * from orders
+-- alter table orders add column address varchar(50);
+-- alter table orders rename column order_id to id;
+-- alter table orders drop column address
+-- update orders set first_name="aashu" where first_name="aarush"
+
+-- select * from orders
+-- delete from orders where id=1
+-- insert into orders values(1,"manish","soni");
+
+-- create table manish122223(names1234 text);
+-- drop table manish122223
+
+-- create table yy(users_no int);
+-- delete from  yy
+-- select * from yy
+ 
+
+-- create database abc
+-- drop database abc
