543401f4e7ebd8a09bec4258ac3e0242d518df6b
Manishsoni2020 <sonimanish24163@gmail.com>
2024-10-09 23:00:52 +0530

    SUB QUERIES

--- sub_query.sql
+++ sub_query.sql
@@ -38,3 +38,93 @@ SELECT emp_name
 FROM Employee e
 WHERE EXISTS (SELECT 1 FROM Employee WHERE dept_id = e.dept_id AND salary >
 60000);
+
+
+-- 4. Subquery in SELECT Clause
+-- Task: Display the name and salary of each employee, along with the average salary in their
+-- department.
+select a.emp_name,a.salary,(select avg(salary) as avg_salary 
+from employee where dept_id=a.dept_id) as avg_dept_salary 
+from employee as a;
+
+-- 5. Subquery with JOIN Clause
+-- Task: Find the names of employees and the names of their departments, where their salary is
+-- greater than the average salary in their department. 
+select employee.emp_name,departments.dept_name from 
+employee inner join departments
+on employee.dept_id=departments.dept_id
+where employee.salary>(select avg(salary) as avg_salary from employee);
+
+-- 6. Correlated Subquery
+-- Task: Find employees whose salary is greater than the average salary of their own department.
+select emp_name,salary from employee
+where salary>(select avg(salary) as avg_salary from employee);
+
+-- --------------------
+-- Practice Exercises
+
+-- 1. Subquery for Maximum Salary: 
+-- Write a query that finds all employees who earn the highest
+-- salary in the company.
+select emp_name 
+from employee
+where salary= (select salary from employee order by salary desc limit 1);
+
+-- 2. 
+-- Subquery for Department with the Most Employees: 
+-- Find the department with the highest number of employees.
+-- select dept_name from departments
+-- where (select count(emp_id) as No_emp from employee );
+
+-- 3. 
+-- Subquery with NOT IN: 
+-- Find the employees who do not belong to department 102.
+select emp_name 
+from employee
+where dept_id not in (select dept_id from departments where dept_id=102);
+
+-- 4. 
+-- Find Departments with Multiple Employees: 
+-- Write a query to list all departments that have
+-- more than one employee.
+select dept_id,count(*) as all_emp from employee
+group by dept_id
+having count(*)>1;
+
+-- 5.
+-- Subquery with BETWEEN: 
+-- Find employees whose salary is between the average salary and
+-- the highest salary in the company.
+select emp_name,salary from employee
+where salary between (select avg(salary) from employee)
+and (select max(salary) from employee);
+
+-- 6. 
+-- Retrieve the name and salary of employees who have a salary greater than the average salary in
+-- the company.
+select emp_name,salary
+from employee
+where salary>(select avg(salary) from employee);
+
+-- 7. 
+-- Find the employees who work in the department with the maximum number of employees.
+select emp_name 
+from employee
+where dept_id = (select dept_id from employee group by dept_id order by count(*) desc limit 1);
+
+-- 8. 
+-- Find the average salary for each department from the employees table.
+select departments.dept_name,avg(employee.salary) as avg_salary 
+from employee inner join departments
+on employee.dept_id=departments.dept_id
+group by departments.dept_name;
+
+-- 9. 
+-- List the names of employees who have a salary greater than the salary of any employee in the
+-- HR department.
+select emp_name 
+from employee
+where salary>(select max(employee.salary) as max_salary 
+from employee inner join departments 
+on employee.dept_id=departments.dept_id 
+where departments.dept_name="HR" );
